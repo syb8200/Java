567073e92cf89498ca28c1a50f6bb65b2ac8aab7
syb8200 <syb8200@naver.com>
2022-01-06 18:36:37 +0900

    Java 시작하기

--- First/src/first/HelloWorld.java
+++ First/src/first/HelloWorld.java
@@ -3,8 +3,8 @@ package first;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+
+		System.out.println("Hello, World!");
 	}
 	
 }
